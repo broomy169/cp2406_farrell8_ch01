662f03a61cfe877734b0e5d8c190116f555000e9
broomy169 <broomyg93@gmail.com>
2016-08-04 12:58:12 +1000

    added programming exercises

---
+++ CodeInFigures/MovieQuote.java
@@ -0,0 +1,9 @@
+public class MovieQuote {
+    public static void main(String[] args) {
+        System.out.println("Because we have to chase him. ");
+        System.out.println("Because he's the hero Gotham deserves, ");
+        System.out.println("but not the one it needs right now, so we'll hunt him. ");
+        System.out.println("Because he can take it, because he's not a hero. ");
+        System.out.println("He's a silent guardian, a watchful protector, a Dark Knight");
+    }
+}
---
+++ CodeInFigures/MovieQuoteInfo.java
@@ -0,0 +1,10 @@
+public class MovieQuoteInfo {
+    public static void main(String[] args) {
+        System.out.println("Because we have to chase him. ");
+        System.out.println("Because he's the hero Gotham deserves, ");
+        System.out.println("but not the one it needs right now, so we'll hunt him. ");
+        System.out.println("Because he can take it, because he's not a hero. ");
+        System.out.println("He's a silent guardian, a watchful protector, a Dark Knight");
+        System.out.println("-Dark Knight - Gordon (2008)");
+    }
+}
